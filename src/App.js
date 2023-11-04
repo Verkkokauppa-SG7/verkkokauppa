28be3044780571d4bf3d29d9e68764e0030bf7f9
IlariHietala <c3hiil00@students.oamk.fi>
2023-11-04 11:37:22 +0200

    Projektinpohjaluonnos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    return (
+      <div>
+        <header>
+          <h1>Verkkokauppa</h1>
+          <nav>
+            <ul>
+              <li><a href="/">Etusivu</a></li>
+              <li><a href="/vaatteet">Vaatteet</a></li>
+              <li><a href="/kengät">Kengät</a></li>
+              <li><a href="/laukut">Laukut</a></li>
+              <li><a href="/asusteet">Asusteet</a></li>
+              <li><a href="/yhteystiedot">Yhteystiedot</a></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <section>
+            <h2>Suosituimmat tuotteet</h2>
+            <div className="tuotteet"></div>
+          </section>
+        </main>
+        <main>
+          <section>
+            <h2>Alennukset</h2>
+            <div className="tuotteet"></div>
+          </section>
+        </main>
+        <footer>
+          <p>&copy; 2023 SG7-Verkkokauppa</p>
+        </footer>
+      </div>
+    );
+  }
+
 
 export default App;
