333098501dc98b6b9ad9aafacc79131ddd50156f
IlariHietala <c3hiil00@students.oamk.fi>
2023-12-06 21:02:26 +0200

    Koodin siistiminen

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,4 +1,3 @@
-// Lisää tarvittavat import-lauseet
 import React, { useState, useEffect } from 'react';
 import '../styles/FeedbackList.css'
 
@@ -49,7 +48,7 @@ const FeedbackList = () => {
   }
 
   return (
-    <div>
+    <div>y
       <h2>Asiakaspalaute</h2>
       <ul className="ulli">
         {feedback.map(item => (
@@ -70,51 +69,3 @@ const FeedbackList = () => {
 };
 
 export default FeedbackList;
-
-
-
-/*import React, { useState, useEffect } from 'react';
-import '../styles/FeedbackList.css'
-
-const FeedbackList = () => {
-  const [feedback, setFeedback] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Haetaan asiakaspalaute palvelimelta
-    fetch('http://localhost:3001/feedback')
-      .then(response => response.json())
-      .then(data => {
-        setFeedback(data);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error('Error fetching feedback:', error);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) {
-    return <p>Ladataan...</p>;
-  }
-
-  return (
-    <div>
-      <h2>Asiakaspalaute</h2>
-      <ul>
-        {feedback.map(item => (
-          <li key={item.id}>
-            <p>Nimi: {item.name}</p>
-            <p>Sähköposti: {item.email}</p>
-            <p>Viesti: {item.message}</p>
-            <hr />
-          </li>
-        ))}
-      </ul>
-      <div className="empty-space"></div>
-    </div>
-
-  );
-};
-
-export default FeedbackList; */
