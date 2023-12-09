e1a102f1f6a5dd0db12a297c69d020c42bdb9883
katrihumaloja <n1huka00@students.oamk.fi>
2023-12-09 20:12:29 +0200

    Lisätty ostoskoriin toiminnallisuutta

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -10,8 +10,8 @@ const ShoppingCart = () => {
       <h1>Ostoskori</h1>
       <CartContents />
     </div>
-  );
-};
+  )
+}
 
 export default ShoppingCart;
 
@@ -21,16 +21,38 @@ function CartContents() {
   // Haetaan ostoskorin tuotteet cartSignalista tai käytetään tyhjää taulukkoa
   const cartProducts = cartSignal.value || []
 
+  // Lasketaan ostoskorin yhteyshinta
   const calculateTotalPrice = () => {
-    return cartProducts.reduce((total, p) => total + (p.count * p.price), 0).toFixed(2);
-  };
+    return cartProducts.reduce((total, p) => total + (p.count * p.price), 0).toFixed(2)
+  }
+
+  // Lasketaan kunkin tuotteen yhteyshinta
+  const calculateProductTotal = (p) => {
+    return (p.count * p.price).toFixed(2)
+  }
+
+  // Vähennetään tai lisätään ostoskorissa olevien tuotteiden määrää
+  const handleIncrement = (productId) => {
+    const updatedCart = cartProducts.map((p) =>
+      p.id === productId ? { ...p, count: p.count + 1 } : p
+    )
+    cartSignal.value = updatedCart
+  }
+
+  const handleDecrement = (productId) => {
+    const updatedCart = cartProducts.map((p) =>
+      p.id === productId ? { ...p, count: p.count > 1 ? p.count - 1 : 0 } : p
+    )
+    const filteredCart = updatedCart.filter((p) => p.count > 0)
+    cartSignal.value = filteredCart
+  }
 
   if (!Array.isArray(cartProducts) || cartProducts.length === 0) {
     return (
       <div>
         <h3>Ostoskorisi on tyhjä</h3>
       </div>
-    );
+    )
   }
 
   return (
@@ -38,22 +60,32 @@ function CartContents() {
       <div className="cart-items">
         {cartProducts.map((p) => (
           <Card key={p.id} className="cart-item">
-            <Card.Body>
-              <Card.Title>{p.product_name}</Card.Title>
-              <Card.Text>
-                Määrä: {p.count}
-              </Card.Text>
-              <Card.Text>
-                Hinta: {p.price}
-              </Card.Text>
+            <Card.Body className="cart-item-body">
+              <img src={process.env.PUBLIC_URL + '/images/' + p.image_url} alt={p.product_name} className="cart-image" />
+              <div className="cart-item-details">
+                <Card.Title>{p.product_name}</Card.Title>
+                <Card.Text>
+                  <p>{p.product_description}</p>
+                </Card.Text>
+                <Card.Text>
+                  <button className="decrement-button" onClick={() => handleDecrement(p.id)}>-</button>
+                  <span> {p.count} </span>
+                  <button className="increment-button" onClick={() => handleIncrement(p.id)}>+</button>
+                </Card.Text>
+              </div>
+              <div className="price">
+                <Card.Text>
+                  {calculateProductTotal(p)} €
+                </Card.Text>
+              </div>
             </Card.Body>
           </Card>
         ))}
       </div>
       <div className="cart-summary">
-        <button className="custom-button" onClick={() => cartSignal.value = []}>Tyhjennä ostoskori</button>
+        <button className="custom-button" onClick={() => (cartSignal.value = [])}>Tyhjennä ostoskori</button>
         <h4 className="total-price">Tilaus yhteensä: {calculateTotalPrice()} €</h4>
       </div>
     </div>
-  );
+  )
 }
