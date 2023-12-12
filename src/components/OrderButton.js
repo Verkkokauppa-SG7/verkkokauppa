21926599b855df5f9f8308a536af09365ed51fa2
smarosa <127884465+smarosa@users.noreply.github.com>
2023-12-12 17:38:44 +0200

    OrderButton update

--- src/components/OrderButton.js
+++ src/components/OrderButton.js
@@ -5,9 +5,8 @@ import { jwtToken } from "./TokenSignal";
 const OrderButton = () => {
 
 //Funktio tarkistaa onko asiakas sisäänkirjautunut ja näyttää eri painiketta sen perusteella.
-//Tämä ei vielä toimi oikein: ei pysty tunnistamaan onko asiakas sisäänkirjautunut
     function checkLogin() {
-        if (jwtToken.value !== '' || jwtToken.value !== 0) {
+        if (jwtToken.value !== '' && jwtToken.value !== 0) {
             return (
                 <Link to="/tilaaminen">
                     <button>Tilaa tuotteet</button>
